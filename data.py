0079619e3a8c886021b0ae39919bc05b84637285
M-Asch <Aschme_m1@denison.edu>
2022-06-30 11:22:55 +0200

    basic model file

--- data.py
+++ data.py
@@ -20,13 +20,13 @@ def findFlags(boards, choices):
     for c in range(len(choices) - 1):
         coords = choices[c]
         if boards[c+1][coords[0]][coords[1]] == 9999:
-            print("on board ", c, " we flagged spot ", coords, " which is equal to spot: ", 400 + 20*coords[0] + coords[1], "\n")
+            #print("on board ", c, " we flagged spot ", coords, " which is equal to spot: ", 400 + 20*coords[0] + coords[1], "\n")
             outputs.append(400 + 20*coords[0] + coords[1])
         elif boards[c+1][coords[0]][coords[1]] == -9999 and boards[c][coords[0]][coords[1]] == 9999:
-            print("on board ", c, " we unflagged spot ", coords, " which is equal to spot: ", 800 + 20*coords[0] + coords[1], "\n")
+            #print("on board ", c, " we unflagged spot ", coords, " which is equal to spot: ", 800 + 20*coords[0] + coords[1], "\n")
             outputs.append(800 + 20*coords[0] + coords[1])
         else:
-            print("on board ", c, " we clicked spot ", coords, " which is equal to spot: ", 20*coords[0] + coords[1], "\n" "\n")
+            #print("on board ", c, " we clicked spot ", coords, " which is equal to spot: ", 20*coords[0] + coords[1], "\n" "\n")
             outputs.append(20*coords[0] + coords[1])
 
     return(outputs)
@@ -34,7 +34,7 @@ def findFlags(boards, choices):
 #boa is a list of boards where each board is a 2D matrix row*col
 #cho is a list of the choices made
 #outputs is the spot that was chosen
-boa, cho = cleanData(file)
-outputs = findFlags(boa, cho)
+#boa, cho = cleanData(file)
+#outputs = findFlags(boa, cho)
 
-print(len(boa[:-1]), len(outputs))
+#print(len(boa[:-1]), len(outputs))
